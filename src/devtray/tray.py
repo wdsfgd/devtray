03542f42d3784e0b4936ddb5f189e8319041022b
wdsfgd <195256786+wdsfgd@users.noreply.github.com>
2026-07-30 16:04:29 +0700

    feat: implement Main Window CRUD (Ticket 04)

--- src/devtray/tray.py
+++ src/devtray/tray.py
@@ -11,10 +11,14 @@ except ValueError:
 
 from gi.repository import Gtk, GLib
 from devtray.task_manager import TaskManager, Task
+from devtray.main_window import MainWindow
 
 class DevTrayApp:
     def __init__(self, task_manager: TaskManager):
         self.task_manager = task_manager
+        
+        # Setup Main Window
+        self.main_window = MainWindow(self.task_manager, self.build_menu)
         
         # Setup AppIndicator
         self.indicator = AppIndicator.Indicator.new(
@@ -30,6 +34,12 @@ class DevTrayApp:
     def build_menu(self):
         menu = Gtk.Menu()
         
+        item_open = Gtk.MenuItem(label="Buka Main Window")
+        item_open.connect("activate", self.on_open_clicked)
+        menu.append(item_open)
+        
+        menu.append(Gtk.SeparatorMenuItem())
+        
         tasks = self.task_manager.get_tasks()
         
         if not tasks:
@@ -43,7 +53,6 @@ class DevTrayApp:
                 label_text = f"{status_icon} {task.name}"
                 
                 item = Gtk.MenuItem(label=label_text)
-                # Pass the task object to the handler
                 item.connect("activate", self.on_task_toggled, task)
                 menu.append(item)
                 
@@ -56,6 +65,11 @@ class DevTrayApp:
         menu.show_all()
         self.indicator.set_menu(menu)
         
+    def on_open_clicked(self, widget):
+        self.main_window.update_ui()
+        self.main_window.show_all()
+        self.main_window.present()
+        
     def on_task_toggled(self, widget, task: Task):
         if self.task_manager.is_running(task):
             self.task_manager.stop_task(task)
@@ -64,6 +78,10 @@ class DevTrayApp:
             
         # Rebuild the menu to reflect the new state immediately
         self.build_menu()
+        
+        # Also update main window if it's visible
+        if self.main_window.get_visible():
+            self.main_window.update_ui()
         
     def on_quit_clicked(self, widget):
         dialog = Gtk.MessageDialog(
