2911d001bb4ffae107e43ec72a3dd2e76db59896
wdsfgd <195256786+wdsfgd@users.noreply.github.com>
2026-08-21 22:01:31 +0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate devtray;
+
 use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
 
 fn main() {
+    cxx_qt::init_crate!(devtray);
+
     let mut app = QGuiApplication::new();
     let mut engine = QQmlApplicationEngine::new();
 
